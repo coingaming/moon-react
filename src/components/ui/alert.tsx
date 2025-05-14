31dbf023bde48c9a5239cdad4e011581fda747eb
Roberto Angulo <roberto.angulo@yolo.com>
2025-05-14 15:54:11 +0300

    feat: add firsts 9 components [MDS-1817]

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,23 +1,24 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const alertVariants = cva(
   "relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-3 gap-y-0.5 items-start [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",
   {
     variants: {
       variant: {
-        default: "bg-card text-card-foreground",
-        destructive:
-          "text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive/90",
+        neutral: "moon-alert-neutral",
+        negative: "moon-alert-negative",
+        caution: "moon-alert-caution",
+        info: "moon-alert-info",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "neutral",
     },
   }
-)
+);
 
 function Alert({
   className,
@@ -28,23 +29,24 @@ function Alert({
     <div
       data-slot="alert"
       role="alert"
-      className={cn(alertVariants({ variant }), className)}
+      className={cn("moon-alert", alertVariants({ variant }), className)}
       {...props}
     />
-  )
+  );
 }
 
 function AlertTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="alert-title"
-      className={cn(
+      /* className={cn(
         "col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight",
         className
-      )}
+      )} */
+      className="moon-alert-title"
       {...props}
     />
-  )
+  );
 }
 
 function AlertDescription({
@@ -60,7 +62,7 @@ function AlertDescription({
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Alert, AlertTitle, AlertDescription }
+export { Alert, AlertTitle, AlertDescription };
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,36 +10,26 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+        fill: "moon-button-fill",
+        soft: "moon-button-soft",
+        outline: "moon-button-outline",
+        ghost: "moon-button-ghost",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
+        xs: "moon-button-xs",
+        sm: "moon-button-sm",
+        md: "moon-button-md",
+        lg: "moon-button-lg",
+        xl: "moon-button-xl",
       },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
     },
   }
 );
 
 function Button({
   className,
-  variant,
-  size,
+  variant = "fill",
+  size = "md",
   asChild = false,
   ...props
 }: React.ComponentProps<"button"> &
@@ -51,8 +41,10 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      // className={cn(buttonVariants({ variant, size, className }))}
-      className="moon-button"
+      className={cn(
+        "moon-button",
+        buttonVariants({ variant, size, className })
+      )}
       {...props}
     />
   );
