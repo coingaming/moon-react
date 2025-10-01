5768bb7e7d7a3c49a229233d2ca1d399d931cfa8
Nadezda Barinova <nadezhda.barinova@gmail.com>
2025-10-01 15:08:01 +0300

    fix: improve circular progress component

--- packages/src/components/CircularProgress.tsx
+++ packages/src/components/CircularProgress.tsx
@@ -10,16 +10,19 @@ type CircularProgressType = {
   size?: CircularProgressSizes;
   value: number;
   className?: string;
+  label?: string;
 };
 
 const CircularProgress = ({
   size = "md",
   className,
   value,
+  label = "moon-circular-progress",
 }: CircularProgressType) => (
   <div
     data-value={value}
     role="progressbar"
+    aria-label={label}
     className={mergeClasses(
       "moon-circular-progress",
       size !== "md" && `moon-circular-progress-${size}`,
